823d62447d337bef5b64c356c02cc9184788eaaa
Matt Schwager <schwag09@gmail.com>
2020-01-08 11:55:46 -0700

    Use error code instead of static 'dlint' in check_id

--- src/schema.py
+++ src/schema.py
@@ -3,7 +3,7 @@ import json
 
 json.dump({
     "results": [
-        {"check_id": "dlint", "path": k, "extra": {"output": v}}
+        {"check_id": v["code"], "path": k, "extra": {"output": v}}
         for k, v in json.load(sys.stdin).items() if v
     ]
 }, sys.stdout)
